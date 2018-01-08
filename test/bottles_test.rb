5a18ccb3f9b1a4b16a61d2e830cefc837c9d6a5e
David Madouros <dmadouros@gospotcheck.com>
2018-01-08 09:25:19 -0700

    Shameless green

--- test/bottles_test.rb
+++ test/bottles_test.rb
@@ -13,7 +13,6 @@ VERSE
   end
 
   def test_another_verse
-    skip
     expected = <<-VERSE
 89 bottles of beer on the wall, 89 bottles of beer.
 Take one down and pass it around, 88 bottles of beer on the wall.
@@ -22,7 +21,6 @@ VERSE
   end
 
   def test_verse_2
-    skip
     expected = <<-VERSE
 2 bottles of beer on the wall, 2 bottles of beer.
 Take one down and pass it around, 1 bottle of beer on the wall.
@@ -31,7 +29,6 @@ Take one down and pass it around, 1 bottle of beer on the wall.
   end
 
   def test_verse_1
-    skip
     expected = <<-VERSE
 1 bottle of beer on the wall, 1 bottle of beer.
 Take it down and pass it around, no more bottles of beer on the wall.
@@ -40,7 +37,6 @@ Take it down and pass it around, no more bottles of beer on the wall.
   end
 
   def test_verse_0
-    skip
     expected = <<-VERSE
 No more bottles of beer on the wall, no more bottles of beer.
 Go to the store and buy some more, 99 bottles of beer on the wall.
@@ -49,7 +45,6 @@ Go to the store and buy some more, 99 bottles of beer on the wall.
   end
 
   def test_a_couple_verses
-    skip
     expected = <<-VERSES
 99 bottles of beer on the wall, 99 bottles of beer.
 Take one down and pass it around, 98 bottles of beer on the wall.
@@ -61,7 +56,6 @@ VERSES
   end
 
   def test_a_few_verses
-    skip
     expected = <<-VERSES
 2 bottles of beer on the wall, 2 bottles of beer.
 Take one down and pass it around, 1 bottle of beer on the wall.
@@ -76,7 +70,6 @@ VERSES
   end
 
   def test_the_whole_song
-    skip
     expected = <<-SONG
 99 bottles of beer on the wall, 99 bottles of beer.
 Take one down and pass it around, 98 bottles of beer on the wall.
